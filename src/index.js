a420c429ebf28e9ab2adbbe12a342d74a2975fe8
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-10 22:01:35 +0300

    creates a list element and pends to the ul

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 
-const form = document.getElementById('create-task-form')
+const form = document.getElementById('create-task-form');
 form.addEventListener('submit', function(e){
-  e.preventDefault()
+  e.preventDefault() // prevents the form from submitting
   
 })
 
+// Gets the task description input value
 const taskInput = document.getElementById('new-task-description');
 const taskDescription = taskInput.value;
+
+ // Creates a new <li> element with the task description
+const taskItem = document.createElement("li");
+taskItem.textContent = taskDescription;
+
+//append the task item to the ul
+const taskList = document.getElementById('tasks');
+taskList.appendChild(taskItem);
