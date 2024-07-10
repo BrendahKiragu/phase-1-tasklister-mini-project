ef0491954064ce89cc0c78bd35dcd8c05185936a
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-10 21:55:57 +0300

    gets the task description input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const form = document.getElementById('create-task-form')
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+  
+})
+
+const taskInput = document.getElementById('new-task-description');
+const taskDescription = taskInput.value;
